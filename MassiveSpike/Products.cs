5774eafa1d41ee97c20930f2c27fd25576771f18
Ken Burkhardt <ken.burkhardt@gmail.com>
2011-10-13 10:13:19 -0400

    starting spike

---
+++ MassiveSpike/Products.cs
@@ -0,0 +1,20 @@
+using Massive;
+
+namespace MassiveSpike
+{
+    public class Products : DynamicModel
+    {
+        //public Products() : base("MassiveSpikeDb", "Products", "ProductId") { }
+
+        public Products() : base("MassiveSpike")
+        {
+            PrimaryKeyField = "ProductId";
+            TableName = "Products";
+        }
+        
+        public Products(string connectionStringName, string tableName, string primaryKeyField, string descriptorField) 
+            : base(connectionStringName, tableName, primaryKeyField, descriptorField)
+        {
+        }
+    }
+}
